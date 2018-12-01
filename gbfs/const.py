b157c816acd282ea75c25a98080f0b1ef6797177
jake1025 <jake1025@gmail.com>
2018-11-30 22:59:19 -0500

    add consts

---
+++ gbfs/const.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+# TODO: Make this dynamic
+root_path = '/Users/jameshadar/dev/GBFSClient'
+
+
+package_root_dirname = 'gbfs'
+package_root_dirpath = os.path.join(root_path, package_root_dirname)
+assert os.path.exists(package_root_dirpath)
+
+package_static_dirname = 'static'
+package_static_dirpath = os.path.join(package_root_dirpath, package_static_dirname)
+assert os.path.exists(package_static_dirpath)
+
+gbfs_systems_csv_remote_url = 'https://raw.githubusercontent.com/NABSA/gbfs/master/systems.csv'
+gbfs_systems_csv_local_filepath = os.path.join(package_static_dirpath, 'systems.csv')
+assert os.path.exists(gbfs_systems_csv_local_filepath)
