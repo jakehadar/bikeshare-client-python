6ffcd3ca082c46edf7c336f6979ccb14e4b9909f
jake1025 <jake1025@gmail.com>
2018-12-01 23:36:32 -0500

    add json fetchers

--- gbfs/data/fetchers.py
+++ gbfs/data/fetchers.py
@@ -1,5 +1,6 @@
 import abc
 import requests
+import json
 
 
 class FileFetcher(metaclass=abc.ABCMeta):
@@ -33,3 +34,33 @@ class RemoteCSVFetcher(FileFetcher):
         return list(response.iter_lines(decode_unicode=True))
 
 RemoteCSVFetcher._requests_module = requests
+
+
+class LocalJSONFetcher(FileFetcher):
+    _json_module = None
+
+    def __init__(self):
+        assert self._json_module
+
+    def fetch(self, url):
+        with open(url, 'r') as f:
+            data = self._json_module.load(f)
+        return data
+
+LocalJSONFetcher._json_module = json
+
+
+class RemoteJSONFetcher(FileFetcher):
+    _requests_module = None
+
+    def __init__(self):
+        assert self._requests_module
+
+    def fetch(self, url):
+        response = self._requests_module.get(url)
+        if response.status_code != 200:
+            raise RuntimeError('HTTPS request for {} failed with status code {}' \
+                               .format(url, response.status_code))
+        return response.json()
+
+RemoteJSONFetcher._requests_module = requests
--- gbfs/tests/data/test_fetchers.py
+++ gbfs/tests/data/test_fetchers.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 
@@ -10,17 +12,23 @@ class dummy_requests_module:
         return self._response
 
 
-class dummy_response_200:
+class dummy_response_csv_200:
     status_code = 200
     def iter_lines(self, *args, **kwargs):
         yield 'Country Code,Name,Location,System ID,URL,Auto-Discovery URL'
         yield 'AE,ADCB Bikeshare,"Abu Dhabi, AE",ABU,https://www.bikeshare.ae/,https://api-core.bikeshare.ae/gbfs/gbfs.json'
 
 
-class dummy_response_404:
+class dummy_response_csv_404:
     status_code = 404
     def iter_lines(self, *ars, **kwargs):
         yield '404: Not Found'
+
+
+class dummy_response_json:
+    status_code = 200
+    def json(self):
+        return {'last_updated': 1543720674, 'ttl': 10, 'data': {'en': {'feeds': [{'name': 'station_status', 'url': 'https://api-core.bikeshare.ae/gbfs/en/station_status.json'}]}}}
 
 
 def test_local_csv_fetcher():
@@ -36,7 +44,7 @@ def test_remote_csv_fetcher_400():
     from gbfs.const import gbfs_systems_csv_remote_url
     from gbfs.data.fetchers import RemoteCSVFetcher
 
-    RemoteCSVFetcher._requests_module = dummy_requests_module(dummy_response_200())
+    RemoteCSVFetcher._requests_module = dummy_requests_module(dummy_response_csv_200())
 
     fetcher = RemoteCSVFetcher()
 
@@ -47,9 +55,28 @@ def test_remote_csv_fetcher_404():
     from gbfs.const import gbfs_systems_csv_remote_url
     from gbfs.data.fetchers import RemoteCSVFetcher
 
-    RemoteCSVFetcher._requests_module = dummy_requests_module(dummy_response_404())
+    RemoteCSVFetcher._requests_module = dummy_requests_module(dummy_response_csv_404())
 
     fetcher = RemoteCSVFetcher()
 
     with pytest.raises(RuntimeError):
         fetcher.fetch(gbfs_systems_csv_remote_url)
+
+
+def test_local_json_fetcher():
+    from gbfs.const import package_tests_fixtures_dirpath
+    from gbfs.data.fetchers import LocalJSONFetcher
+
+    fetcher = LocalJSONFetcher()
+
+    assert fetcher.fetch(os.path.join(package_tests_fixtures_dirpath, 'gbfs.json'))
+
+
+def test_remote_json_fetcher():
+    from gbfs.data.fetchers import RemoteJSONFetcher
+
+    RemoteJSONFetcher._requests_module = dummy_requests_module(dummy_response_json())
+
+    fetcher = RemoteJSONFetcher()
+
+    assert fetcher.fetch('http://path/to/gbfs.json')
