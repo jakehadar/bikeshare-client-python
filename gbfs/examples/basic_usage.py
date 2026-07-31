534be2d8060d4e928bb9cf7d08b56718351cc3de
Kevin Cole <dc.loco@gmail.com>
2026-07-31 16:27:24 -0400

    Make station_status time-zone-aware instead of making now unaware

--- gbfs/examples/basic_usage.py
+++ gbfs/examples/basic_usage.py
@@ -2,6 +2,7 @@ import datetime
 
 
 from gbfs.services import SystemDiscoveryService
+import pytz
 
 def example():
     ds = SystemDiscoveryService()
@@ -53,7 +54,7 @@ def example():
     statuses = station_status.get('data').get('stations')
     first_station_status = [s for s in statuses if s.get('station_id') == first_station.get('station_id')][0]
 
-    staleness = datetime.datetime.now(datetime.UTC).replace(tzinfo=None)  - station_status.get('last_updated')
+    staleness = datetime.datetime.now(datetime.UTC)  - station_status.get('last_updated').replace(tzinfo=pytz.utc)
     print('Station {} has a total capacity of {} and (as of {} seconds ago) has {} bikes available for rent.'.format(
         first_station.get('name'), first_station.get('capacity'), staleness.seconds,
         first_station_status.get('num_bikes_available')))
